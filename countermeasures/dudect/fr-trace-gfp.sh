d3ee6ab30ea816360f762f1df5b0287a13a9450a
Diego F. Aranha <dfaranha@gmail.com>
2020-08-18 23:18:23 +0200

    Update dudect programs.

--- countermeasures/dudect/fr-trace-gfp.sh
+++ countermeasures/dudect/fr-trace-gfp.sh
@@ -1,1 +1,1 @@
-../../cache-attacks/FR-trace -r 10000 -s 5000 -c 10000 -l 500 -p 4 -H -f dudect_secp192r1_-O2 -m ecp_smpl.c:927 -m ecp_smpl.c:934  -t bn_lib.c:514 -t bn_lib.c:543 > /dev/null &
+../../cache-attacks/FR-trace -r 10000 -s 5000 -c 10000 -l 500 -p 8 -H -f dudect_secp192r1_-O2 -m ecp_smpl.c:927 -m ecp_smpl.c:934  -t bn_lib.c:514 -t bn_lib.c:543 > /dev/null &
